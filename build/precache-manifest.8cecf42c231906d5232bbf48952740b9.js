b20bc98a60cef931577bf6d83768e0dbeb2d3138
Connor Pote <cpotey@me.com>
2019-04-10 11:20:59 +0100

    Build folder

---
+++ build/precache-manifest.8cecf42c231906d5232bbf48952740b9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "11ea5efbd1c9e8f273869a6d8487a079",
+    "url": "/static/media/right.11ea5efb.svg"
+  },
+  {
+    "revision": "83f21ff6776ca6eabc95",
+    "url": "/static/css/main.96707042.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "18fbede2de5d80c2d3b0",
+    "url": "/static/js/2.98be1ae0.chunk.js"
+  },
+  {
+    "revision": "5434740cfd131adef8acbdf8fb43ba3a",
+    "url": "/static/media/left.5434740c.svg"
+  },
+  {
+    "revision": "26d5282087f317098eeb631ad36fde9a",
+    "url": "/static/media/placeholder.26d52820.jpg"
+  },
+  {
+    "revision": "83f21ff6776ca6eabc95",
+    "url": "/static/js/main.c9f8b303.chunk.js"
+  },
+  {
+    "revision": "a87eca9ea0f39475f2fc7613a3af312f",
+    "url": "/static/media/search.a87eca9e.svg"
+  },
+  {
+    "revision": "43666b5e46a5d44b8c9457955a403041",
+    "url": "/static/media/AvenirLTStd-Roman.43666b5e.woff"
+  },
+  {
+    "revision": "5ae9e0cbe24b9b3f0fe11d5ad0306521",
+    "url": "/static/media/AvenirLTStd-Medium.5ae9e0cb.woff"
+  },
+  {
+    "revision": "d74d87dd165fc5d52722f8357738a03d",
+    "url": "/static/media/AvenirLTStd-Black.d74d87dd.woff"
+  },
+  {
+    "revision": "82f0917cd0ac07f541fca75cca25c436",
+    "url": "/index.html"
+  }
+];
